fca63f31b2a08cbf491406b3c53d27b44b96590d
SUMAIRA SHAIKH <160852390+SUMAIRA-SHAIKH@users.noreply.github.com>
2024-04-27 02:19:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+console.log("\n\t\t~~~~~~~~Word Counter~~~~~~~~~~\n");
+// import the inquirer module which is a command line interfance for node.js
+import inquirer from "inquirer";
+//declear a constant "answer" and assign it to the result of inquirer.prompt function
+const answers 
+// =  -> inquirer ka istimal karty way(excuation side)
+= await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//const array = ['a','b']
+//print the array of words to the console
+console.log(words);
+//print the word count  of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
